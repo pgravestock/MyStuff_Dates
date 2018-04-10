952e389b65b0c0f2e19613d921846cb21774a867
paulgravestock <pgravestock@gmail.com>
2018-04-11 00:12:33 +0100

    Testing things

---
+++ MyStuff_Dates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyStuff_Dates
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now;
+            DateTime anHourLater = rightNow.AddHours(1);
+
+            //Can we get this to just return the date part, not the empty time stamp part
+            DateTime justDate = DateTime.Today;
+            bool dstSetting = rightNow.IsDaylightSavingTime();
+
+            Console.WriteLine("The contents of rightNow is :" + rightNow);
+            Console.WriteLine("The time one hour later is :" + anHourLater);
+            Console.WriteLine("Just the date is :" + justDate);
+            Console.WriteLine("And the time is :" + rightNow.TimeOfDay);
+            Console.WriteLine("And the DST setting is :" + dstSetting);
+
+            //Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+
+
+        }
+    }
+}
